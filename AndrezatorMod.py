64c1ad1a2bed04cb9f60a0d7c83b73a0092e2a55
BriticHZ <46595671+BriticHZ@users.noreply.github.com>
2020-09-20 01:17:30 +0300

    Update AndrezatorMod.py

--- AndrezatorMod.py
+++ AndrezatorMod.py
@@ -60,9 +60,9 @@ class AndrezatorMod(loader.Module):
 		draw.multiline_text((20, 20),t,(0,0,0),font=font, align='left')
 		imtext.thumbnail((339, 181))
 		w, h = 339, 181
-		img.paste(imtext, (10,10), imtext)
+		img.paste(imtext, (20,20), imtext)
 		out = io.BytesIO()
-		out.name = "@sad0ff.jpg"
+		out.name = "@br1t1c.jpg"
 		img.save(out)
 		out.seek(0)
 		await message.client.send_file(message.to_id, out, reply_to=reply)
