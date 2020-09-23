7e06a92f12ca36e2ae3a5d107f23246cb984e3d3
BriticHZ <46595671+BriticHZ@users.noreply.github.com>
2020-09-23 15:33:47 +0300

    Update and rename Earth.py to chine.py

--- chine.py
+++ chine.py
@@ -1,12 +1,12 @@
-# (c) @UniBorg
-# Original written by @UniBorg edit by @I_m_Rock
+# (c) @br1t1c
+# Original written by @UniBorg edit by @br1t1c
 
 from telethon import events
 import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.earth", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.china", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
